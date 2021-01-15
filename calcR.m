acc3f108498fb95832a10bef7ea7119b39603689
Ken H. Andersen <kha@aqua.dtu.dk>
2021-01-15 09:51:20 +0100

    Fixed handling of O2

--- calcR.m
+++ calcR.m
@@ -2,6 +2,7 @@ function r = calcR(p, L, P, Fe)
 
 JL = @(L) p.aMax*(1-exp(-p.aL*L/(p.aMax)))-p.aR0;
 JFe = @(Fe) p.aFe*Fe;
+jPh = 0*L;
 
 Plimit = JL(L) / (p.aP*(1+p.betaD+p.betaP));
 
@@ -13,11 +14,11 @@ r(ix) = JL(L(ix)) / (1+p.betaD+p.betaP);
 if p.bPhosphonate
     % ...with phosphonate break down
     r(~ix) = (JL(L(~ix))+p.aP*P*p.betaPh) / (1+p.betaD+p.betaPh+p.betaP);
-    jPh = 0; % NEEDS TO BE FIXED
+    jPh(~ix) = r(~ix) - p.aP*P;
 else
     %...without phosphonate break down
     r(~ix) = min(p.aP*P, JL(L(~ix))-p.aP*P*(p.betaD+p.betaP));
-    jPh = 0;
+    jPh(~ix) = 0;
 end
 % Burn the remaining O2 and reduce growth accordingly:
 jRtot = p.aR0 + p.betaD*r + p.betaP*p.aP*P + (p.betaPh+p.betaP)*jPh;
