77015f6b1580e2cadae271cd8c7f1acb18009f34
Ken H. Andersen <kha@aqua.dtu.dk>
2021-01-23 11:41:19 +0100

    Added main figure plot

--- panelSizeRates.m
+++ panelSizeRates.m
@@ -30,26 +30,40 @@ hold on
 fillbetweenplot(z, min(r,jP), min(jP+jPh,r), [0,0,1])
 
 ix = jP<=r;
-semilogy(z(ix), jP(ix), 'k--','linewidth',5)
+semilogy(z(ix), jP(ix), 'k:','linewidth',3)
 
-fillbetweenplot(z, jL, min(jL,jL-jRtot+p.aR0), [0.5,0,0]);
+fillbetweenplot(z, jL, min(jL,jL-jRtot+p.aR0), [0.75,0,0]);
 fillbetweenplot(z, min(jL,jL-jRtot+p.aR0), jL-jRtot+p.aR0-jO2, [1,0,0]);
 
 semilogx(z, r, 'k-','linewidth',5)
-semilogx(z, jL, 'g-','linewidth',3)
+semilogx(z, jL, 'color',[0.4,0,0],'linewidth',3)
 semilogx(z, jFe, 'm-','linewidth',2)
 
-semilogx(z, rNoPh, 'y:','linewidth',3)
+%semilogx(z, rNoPh, 'w:','linewidth',3)
 
 plot([1 1],[-0.1, 1],'k--')
 plot(0.4*[1 1],[-0.1, 1],'k--')
-hold off
 
-xlabel('Size ratio z')
-ylabel('Growth rate r (day^{-1})')
+%
+% Add points:
+%
+col = [0.6 0 0.6];
+plot(1, calcR(funcParam(1, true),L,P,Fe)+p.mort, ...
+    'o','markeredgecolor','w','markerfacecolor',col,'markersize',10)
+plot(1, calcR(funcParam(1, false),L,P,Fe)+p.mort, ...
+    'o','markeredgecolor','w','markersize',8)
+
+col = [0 1 0];
+plot(0.4, calcR(funcParam(0.4, false),L,P,Fe)+p.mort, ...
+    'o','markeredgecolor','w','markerfacecolor',col,'markersize',10)
+
+
+
+xlabel('Size relative to Trichodesmium')
+ylabel('Division rate{\it r} (day^{-1})')
 title(sprintf('P=%1.3f  Fe=%1.3f  L=%3.0f',[P,Fe,L]))
 ylim([0, 1])
 
-legend({'Phosphate uptake', 'Phosphonate breakdown',...
-    'Growth w/o phosphonates','Needed respiration','O2 removal',...
+legend({'Phosphate', 'Phosphonate',...
+    'W/o phosphonates','Respiration','O2 removal',...
     'Growth rate','Photoharvesting','Iron uptake'},'box','off')
--- plotMainFigure.m
+++ plotMainFigure.m
@@ -2,7 +2,7 @@ pTri = parameters(1,true);
 pUCYN = parameters(0.4, false); 
 
 P = 0.3;
-Fe = 0.01;
+Fe = 0.015;
 convFe = 1/55*1000;
 convP = 1/31*1000;
 
@@ -10,16 +10,32 @@ clf
 subplot(1,3,[1 2])
 panelSizeRates(150, P, Fe, @parameters)
 title('')
-defaultAxes()
+plotlabel('a',false)
+defaultAxes(20)
+
+ax = gca;
+pos = ax.InnerPosition;
+pos(2) = 0.17;
+ax.InnerPosition = pos;
+
+text(0.11, 0.5, 'O_2 removal','color','w')
+text(0.25, 0.06, 'Phosphate', 'color','w')
+text(0.4,  0.25, ' Phosphonate', 'color','w')
+text(0.25, 0.65, 'Respiration','color','w')
 
 subplot(1,3,3)
 plotDominatType(pTri, pUCYN, 150);
 hold on
-plot(Fe*convFe, P*convP, 'k*')
+plot(Fe*convFe, P*convP, 'kp','markersize',8,'markerfacecolor','k')
 hold off
 %ylabel('')
 %set(gca,'yticklabel','')
-text(0.2, 20, 'UCYN-A')
-text(0.15, 7.5, 'Trichodesmium')
-plotlabel('b', false)
+text(0.2, 25, 'UCYN-A')
+text(0.15, 5.5, 'Trichodesmium')
+plotlabel('  b', false)
 defaultAxes()
+
+ax = gca;
+pos = ax.InnerPosition;
+pos(2) = 0.17;
+ax.InnerPosition = pos;
